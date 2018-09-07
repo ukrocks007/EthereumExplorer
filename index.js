d8c03396a15df80c18fec3fab5d93aa2b2c96e14
Utkarsh Mehta <utkarsh.mehta@klingelnberg.com>
2018-09-07 15:37:03 +0530

    port settings

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ app.get('/eth/api/v1/transaction/:TXID', (req, res) => {
         });
 })
 
-var server = app.listen(8000, function () {
+var server = app.listen(process.env.PORT ? process.env.PORT : 8000, function () {
 
     var host = server.address().address
     var port = server.address().port
